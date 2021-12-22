ba3e1a03396c2e667a3e051dd7017900fbb4230d
Tenekonate <47537111+Tenekonate@users.noreply.github.com>
2021-12-22 17:12:34 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Damier from "./components/Damier";
 function App() {
+  const [addRows, setAddRows] = useState(7);
+  const [addCols, setAddCols] = useState(7);
+
   return (
     <div className="App">
-      <Damier rows={12} cols={7} />
+      <Damier rows={addRows} cols={addCols} />
+      <button value={addRows} onClick={(e) => setAddRows(addRows + 1)}>
+        AddRows {addRows} +
+      </button>
+      <button value={addCols} onClick={(e) => setAddCols(addCols + 1)}>
+        AddCols {addCols} +
+      </button>
     </div>
   );
 }
