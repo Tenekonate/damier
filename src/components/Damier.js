1c5e3ba65ce7f3f1bac9ca0e3ec3f3ba769ce082
Tenekonate <47537111+Tenekonate@users.noreply.github.com>
2021-12-22 17:11:26 +0100

    Add files via upload

--- src/components/Damier.js
+++ src/components/Damier.js
@@ -1,41 +1,49 @@
-import React,{useState} from "react";
+import React from "react";
 
-const Damier = ({rows, cols}) => {
-     const [rows, setRows] = useState(0);
-    
-    const damier= [];
-    const drows = [];
-    console.log(rows);
+const Damier = ({ rows, cols }) => {
+  const damier = [];
+  const even = [];
+  const odd = [];
 
-     
-    for (let r = 1; r <= rows ; r++) {
-        drows.push(<td style={{
-                backgroundColor: r % 2 == 0 ? 'black'}}
-                key={r}>" "</td>);
-        if( r % 2 == 0) {
-            
-        }
-        else{
-            drows.push(<td key={r}>{r}</td>);   
-        }
-    
-        }
-    for (let c = 1; c <= cols ; c++) {
-              
-        damier.push(<tr key={c}>{c} {drows}</tr>)
+  for (let r = 1; r <= rows; r++) {
+    even.push(
+      <td
+        style={{
+          width: 15,
+          height: 15,
+          backgroundColor: r % 2 === 0 ? "black" : "red",
+        }}
+        key={r}
+      ></td>
+    );
+  }
+
+  for (let r = 1; r <= rows; r++) {
+    odd.push(
+      <td
+        style={{
+          width: 15,
+          height: 15,
+          backgroundColor: r % 2 === 0 ? "red" : "black",
+        }}
+        key={r}
+      ></td>
+    );
+  }
+
+  for (let c = 1; c <= cols; c++) {
+    if (c % 2 === 0) {
+      damier.push(<tr key={c}>{even}</tr>);
+    } else {
+      damier.push(<tr key={c}>{odd}</tr>);
     }
+  }
 
-
-    return (
-        <div>
-            <table>
-                <tbody>
-                   {damier}
-                </tbody>
-            </table>
-            <button value={rows} onClick ={ ((e) => rows + 1) }>{rows} +</button>
-        </div>
-    )
-}
+  return (
+    <table>
+      <tbody>{damier}</tbody>
+    </table>
+  );
+};
 
 export default Damier;
